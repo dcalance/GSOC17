0dd5396f7504f10be31df68de8b6cac88eb6a142
dcalance <daniel.calancea@faf.utm.md>
2017-08-07 14:40:58 +0300

    Class for handling the arguments to the tool

---
+++ CSCache/LibArgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Options;
+using System.IO;
+
+namespace CSCacheLib
+{
+    class LibArgs
+    {
+        public static void ShowHelp(OptionSet p)
+        {
+            Console.WriteLine("Usage: CSCache [COMPILE COMMAND] [OPTIONS]");
+            Console.WriteLine("Example : CSCache \"mcs input1.cs -r:lib1.dll\" -clear=all");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            p.WriteOptionDescriptions(Console.Out);
+        }
+        public static void ClearCache(string path) //clearing older cache based on input nr, 0 for all
+        {
+            DirectoryInfo info = new DirectoryInfo(path);
+            FileInfo[] files = info.GetFiles().Where(x => x.Extension == ".cache").OrderByDescending(p => p.CreationTime).ToArray();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                files[i].Delete();
+            }
+        }
+    }
+}
