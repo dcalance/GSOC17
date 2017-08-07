5f4c9d697895977afbcadf29174771d3bdf125ef
dcalance <daniel.calancea@faf.utm.md>
2017-08-07 14:41:22 +0300

    Slight modifications to the tool

--- CSCache/buildLib.bat
+++ CSCache/buildLib.bat
@@ -1,1 +1,1 @@
-mcs -r:System.Configuration.dll -r:Options.dll -t:library Config.cs ConsoleTools.cs CSCacheLib.cs FilesTools.cs MD5Tools.cs -out:CSCacheLib.dll & pause
+mcs -r:System.Configuration.dll -r:Options.dll -t:library Config.cs ConsoleTools.cs CSCacheLib.cs FilesTools.cs MD5Tools.cs LibArgs.cs -out:CSCacheLib.dll & pause
